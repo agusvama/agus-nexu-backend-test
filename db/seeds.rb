6e5d4f979188c4e2a46bb4c90dfc966854b60b39
Agus Vama <vama.agus@gmail.com>
2025-07-24 14:37:35 -0600

    Populating the database w/ the included json

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,27 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'json'
+
+file_path = Rails.root.join("db/seeds/models.json")
+models = JSON.parse(File.read(file_path))
+
+by_brand = models.group_by { |m| m["brand_name"] }
+
+by_brand.each do |brand_name, models|
+  prices = models.map { |m| m["average_price"] }
+  average_brand_price = prices.sum / prices.size unless prices.empty?
+
+  brand = Brand.create!(
+    name: brand_name,
+    average_price: average_brand_price.to_i,
+  )
+  puts "Created brand: #{brand_name}"
+
+  models.each do |model|
+    brand.models.create!(
+      name: model["name"],
+      average_price: model["average_price"],
+    )
+  end
+end
